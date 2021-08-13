b918779e758757399fcdb013834e74d0ee03a00b
Ltibbe <88551349+Ltibbe@users.noreply.github.com>
2021-08-13 12:55:55 +0200

    Added variables, arrays and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,46 @@
-alert('Hello world');
-
-let favoriteFood = "Asian";
-document.write(favoriteFood);
+pokemonList[{
+    name: 'Bulbasaur',
+    height: '7',
+    type: ['grass', 'poison'],
+    abilities: ['chlorophyll', 'overgrow']
+}, {
+    name: 'Ninetales',
+    height: '1.1',
+    type: 'fire',
+    abilities: ['flash fire', 'draught']
+}, {
+    name: 'Ivysaur',
+    height: '1',
+    type: ['grass', 'poison'],
+    abilities: ['chlorophyll', 'overgrow']
+}, {
+    name: 'charmander',
+    height: '0.6',
+    type: 'fire',
+    abilities: ['blaze', 'solar-power']
+}, {
+    name: 'Charmeleon',
+    height: '1.1',
+    type: 'fire',
+    abilities: ['blaze', 'solar-power']
+}, {
+    name: 'Wartortle',
+    height: '1',
+    type: 'water',
+    abilities: ['rain dish', 'torrent']
+}, {
+    name: 'Squirtle',
+    height: '0.5',
+    type: 'water',
+    abilities: ['rain-dish', 'torrent']
+}, {
+    name: 'Charizard',
+    height: '1.7',
+    type: ['fire', 'flying'],
+    abilities: ['Blaze', 'Solar-power']
+}, {
+    name: 'Blastoise',
+    height: '1.6',
+    type: 'water',
+    abilities: ['rain-dish', 'torrent']
+}];
