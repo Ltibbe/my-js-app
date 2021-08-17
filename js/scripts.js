2d1f41fab4d35a6931545569de74ea0b86373166
Ltibbe <88551349+Ltibbe@users.noreply.github.com>
2021-08-17 17:36:53 +0200

    added let PokemonList and edited 'for' code

--- js/scripts.js
+++ js/scripts.js
@@ -57,8 +57,64 @@ var PokemonList = [
 
 //!* Writes the name of the pokemon, and highlights special pokemon sizes *//
 
-let PokemonList = "";
-for (let i = 0; i < 8; PokemonList.length; i++) {
+let PokemonList = [
+    {
+        name: "Bulbasaur",
+        height: "7",
+        types: ["grass", "poison"],
+        abilities: ["chlorophyll", "overgrow"]
+        },
+    {
+        name: "Ninetales",
+        height: "1.1",
+        types: "fire",
+        abilities: ["flash fire", "draught"]
+        },
+    {
+        name: "Ivysaur",
+        height: "1",
+        types: ["grass", "poison"],
+        abilities: ["chlorophyll", "overgrow"]
+        },
+    {
+        name: "charmander",
+        height: "0.6",
+        types: "fire",
+        abilities: ["blaze", "solar-power"]
+        },
+    {
+        name: "Charmeleon",
+        height: "1.1",
+        types: "fire",
+        abilities: ["blaze", "solar-power"]
+        },
+    {
+        name: "Wartortle",
+        height: "1",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
+        },
+    {
+        name: "Squirtle",
+        height: "0.5",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
+        },
+    {
+        name: "Charizard",
+        height: "1.7",
+        types: ["fire", "flying"],
+        abilities: ["blaze", "solar-power"]
+        },
+    {
+        name: "Blastoise",
+        height: "1.6",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
+        }
+    ];
+
+for (let i = 0; i < PokemonList.length; i++) {
     if (PokemonList[i].height > 6) {
         document.write("<p>" + PokemonList[i].name + "("
             PokemonList[i].height ")"
