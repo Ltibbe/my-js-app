8b4c5f9eec9a0da17aad30a6a73be131ac0069a1
Ltibbe <88551349+Ltibbe@users.noreply.github.com>
2021-08-17 16:30:41 +0200

    changed ' to " in array and created a loop w.let/for

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,76 @@
 var PokemonList = [
     {
-        name: 'Bulbasaur',
-        height: '7',
-        type: ['grass', 'poison'],
-        abilities: ['chlorophyll', 'overgrow']
+        name: "Bulbasaur",
+        height: "7",
+        types: ["grass", "poison"],
+        abilities: ["chlorophyll", "overgrow"]
         },
     {
-        name: 'Ninetales',
-        height: '1.1',
-        type: 'fire',
-        abilities: ['flash fire', 'draught']
+        name: "Ninetales",
+        height: "1.1",
+        types: "fire",
+        abilities: ["flash fire", "draught"]
         },
     {
-        name: 'Ivysaur',
-        height: '1',
-        type: ['grass', 'poison'],
-        abilities: ['chlorophyll', 'overgrow']
+        name: "Ivysaur",
+        height: "1",
+        types: ["grass", "poison"],
+        abilities: ["chlorophyll", "overgrow"]
         },
     {
-        name: 'charmander',
-        height: '0.6',
-        type: 'fire',
-        abilities: ['blaze', 'solar-power']
+        name: "charmander",
+        height: "0.6",
+        types: "fire",
+        abilities: ["blaze", "solar-power"]
         },
     {
-        name: 'Charmeleon',
-        height: '1.1',
-        type: 'fire',
-        abilities: ['blaze', 'solar-power']
+        name: "Charmeleon",
+        height: "1.1",
+        types: "fire",
+        abilities: ["blaze", "solar-power"]
         },
     {
-        name: 'Wartortle',
-        height: '1',
-        type: 'water',
-        abilities: ['rain-dish', 'torrent']
+        name: "Wartortle",
+        height: "1",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
         },
     {
-        name: 'Squirtle',
-        height: '0.5',
-        type: 'water',
-        abilities: ['rain-dish', 'torrent']
+        name: "Squirtle",
+        height: "0.5",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
         },
     {
-        name: 'Charizard',
-        height: '1.7',
-        type: ['fire', 'flying'],
-        abilities: ['blaze', 'solar-power']
+        name: "Charizard",
+        height: "1.7",
+        types: ["fire", "flying"],
+        abilities: ["blaze", "solar-power"]
         },
     {
-        name: 'Blastoise',
-        height: '1.6',
-        type: 'water',
-        abilities: ['rain-dish', 'torrent']
+        name: "Blastoise",
+        height: "1.6",
+        types: "water",
+        abilities: ["rain-dish", "torrent"]
         }
     ];
+
+let PokemonListHeight = "";
+for (let i = 0; i < 8; PokemonList.length; i++) {
+    if (PokemonList[i].height > 6) {
+        document.write("<p>" + PokemonList[i].name + "("
+            PokemonList[i].height ")"
+            " - Wow, that’s big!"
+            "</p>");
+    } else if (PokemonList[i].height > 1.5 && < 4) {
+        document.write("<p>" + PokemonList[i].name + "("
+            PokemonList[i].height ")"
+            "is medium size"
+            "</p>");
+    } else {
+        document.write("<p>" + PokemonList[i].name + "("
+            PokemonList[i].height ")"
+            "is the shortest"
+            "</p>");
+    }
+}
