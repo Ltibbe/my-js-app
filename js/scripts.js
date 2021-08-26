998880c7922eaa7a23e8e5f8fab3b5ad9678911a
Ltibbe <88551349+Ltibbe@users.noreply.github.com>
2021-08-26 15:34:07 +0200

    Code isn't working.. Please check!

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,73 @@
-//!* Writes the name of the pokemon, and highlights special pokemon sizes *//
-
-let PokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 7,
-        types: ["grass", "poison"],
-        abilities: ["chlorophyll", "overgrow"]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 7,
+            types: ["grass", "poison"],
+            abilities: ["chlorophyll", "overgrow"]
         },
-    {
-        name: "Ninetales",
-        height: 1.1,
-        types: "fire",
-        abilities: ["flash fire", "draught"]
+        {
+            name: "Ninetales",
+            height: 1.1,
+            types: "fire",
+            abilities: ["flash fire", "draught"]
         },
-    {
-        name: "Ivysaur",
-        height: 1,
-        types: ["grass", "poison"],
-        abilities: ["chlorophyll", "overgrow"]
+        {
+            name: "Ivysaur",
+            height: 1,
+            types: ["grass", "poison"],
+            abilities: ["chlorophyll", "overgrow"]
         },
-    {
-        name: "charmander",
-        height: 0.6,
-        types: "fire",
-        abilities: ["blaze", "solar-power"]
+        {
+            name: "charmander",
+            height: 0.6,
+            types: "fire",
+            abilities: ["blaze", "solar-power"]
         },
-    {
-        name: "Charmeleon",
-        height: 1.1,
-        types: "fire",
-        abilities: ["blaze", "solar-power"]
+        {
+            name: "Charmeleon",
+            height: 1.1,
+            types: "fire",
+            abilities: ["blaze", "solar-power"]
         },
-    {
-        name: "Wartortle",
-        height: 1,
-        types: "water",
-        abilities: ["rain-dish", "torrent"]
+        {
+            name: "Wartortle",
+            height: 1,
+            types: "water",
+            abilities: ["rain-dish", "torrent"]
         },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        types: "water",
-        abilities: ["rain-dish", "torrent"]
+        {
+            name: "Squirtle",
+            height: 0.5,
+            types: "water",
+            abilities: ["rain-dish", "torrent"]
         },
-    {
-        name: "Charizard",
-        height: 1.7,
-        types: ["fire", "flying"],
-        abilities: ["blaze", "solar-power"]
+        {
+            name: "Charizard",
+            height: 1.7,
+            types: ["fire", "flying"],
+            abilities: ["blaze", "solar-power"]
         },
-    {
-        name: "Blastoise",
-        height: 1.6,
-        types: "water",
-        abilities: ["rain-dish", "torrent"]
+        {
+            name: "Blastoise",
+            height: 1.6,
+            types: "water",
+            abilities: ["rain-dish", "torrent"]
         }
     ];
 
-function myLoopFunction(Pokemon) {
-    console.log(Pokemon.name + ' is ' + Pokemon.age + ' years old.');
-}
-PokemonList.forEach(myLoopFunction);
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+})();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add(item)({
+    name: 'Pikachu'
+});
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
