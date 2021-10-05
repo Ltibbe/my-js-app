2bbf581de2ec986499bcc7cbcb6a4be258392907
Layla Tibbe <88551349+Ltibbe@users.noreply.github.com>
2021-10-05 15:06:09 +0200

    added image container

--- js/script.js
+++ js/script.js
@@ -177,6 +177,18 @@ let PokemonRepository = (function () {
   dialogPromiseReject = reject;
 });
 
+let container = document.querySelector('#image-container');
+
+// Create an <img> element
+let myImage = document.createElement('img');
+
+// setting `src` property to set the actual element's `src` attribute
+// this also works on <img> elements selected by querySelector() method, it is not specific for <img> elements created with createElement() methods
+myImage.src = 'https://picsum.photos/300/300';
+
+container.appendChild(myImage);
+});
+
   document.querySelector('#show-modal').addEventListener('click', () => {
   showModal('Modal title', 'This is the modal content!');
 
